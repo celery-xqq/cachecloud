3c9fde2bd64a3af6d8d0bfc23d9f643df887a2c4
cqyisbug <cqyisbug@163.com>
2018-05-16 16:49:09 +0800

    [M] modify out put

--- docker/start.sh
+++ docker/start.sh
@@ -19,7 +19,7 @@ echo "h3c.db.user=${DBUSER}" >> /spec.properties
 echo "h3c.db.password=${DBPWD}" >> /spec.properties
 
 JAVA_OPTS="-server -Xmx1g -Xms1g -Xss256k -XX:MaxDirectMemorySize=1G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:G1ReservePercent=25 -XX:InitiatingHeapOccupancyPercent=40 -XX:+PrintGCDateStamps -Xloggc:/opt/cachecloud-web/logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/cachecloud-web/logs/java.hprof -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+PrintCommandLineFlags  -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.util.Arrays.useLegacyMergeSort=true -Dfile.encoding=UTF-8 -Duser.timezone=GMT+08"
-echo -e "Starting the CacheCloud ...\c"
+echo -e "======================Starting the CacheCloud========================="
 
 cat /spec.properties
 java $JAVA_OPTS -jar /opt/cachecloud-web/web.war --server.port=8080 
